4dc7ac6793d858d786da5bda52d51a2f29a689e9
Shivani Jha <shivjha@iu.edu>
2019-09-20 18:54:53 -0400

    18-Sept

--- app/public/js/recordApp.js
+++ app/public/js/recordApp.js
@@ -1,7 +1,13 @@
 var recordApp= new Vue({
   el: '#recordApp', //points back to object in html; its constraining the script in the scope of the element
   data: { //data for my application
-    patients:[]
+    formPatient: {
+      firstName: '',
+      lastName: '',
+      dob: '',
+      gender: ''
+    },
+    patients: []
   },
   methods: {
     fetchPatients() {
@@ -13,8 +19,21 @@ var recordApp= new Vue({
      //       // fetch('https://randomuser.me/api/')
      //       // .then(function(response) {return response.json()})
      //       // .then(function(json) {waitingApp.people = json});
+    },
+  handleCreateRecord(event) {
+    // fetch(url, {method: 'post', data: this.formPatient})
+    this.patients.push(this.formPatient);
+    this.formPatient = {
+      firstName: '',
+      lastName: '',
+      dob: '',
+      gender: ''
     }
   },
+  handleClick(event) {
+    console.log(event);
+  }
+},
   created() {
     this.fetchPatients();
   }// a function which is envoked when the vue application is created
