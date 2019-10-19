90f45289b6b4cb75ed8281a76a24ef89497dde72
Shivani Jha <shivjha@iu.edu>
2019-10-19 17:52:43 -0400

    UTC date (fixed)

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -8,14 +8,14 @@ var waitingApp = new Vue({
       fetch('api/waiting/index.php')
       .then(response => response.json())
       .then(json => { waitingApp.patients = json })
-    }
-  },
+    },
   displayWaitingLocalDate(d) {
     return moment.utc(d).local().format("HH:mm MMM Do");
   },
   displayWaitingSince(d) {
     return moment.utc(d).local().fromNow();
-  },
+  }
+},
   created() {
     this.fetchPatients();
   }
